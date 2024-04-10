52eea2fbc225e0e1e9211fb791dfb64b73a6f08d
Basirov Ilgiz <Basirov_ilgiz@mail.ru>
2024-04-10 21:38:56 +0500

    create project calculation

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
