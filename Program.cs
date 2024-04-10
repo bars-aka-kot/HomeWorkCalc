8a8722cc5ba339f55e50624346132091776410c8
Basirov Ilgiz <Basirov_ilgiz@mail.ru>
2024-04-10 22:03:24 +0500

    add code to program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length == 3)
+            {
+                double num1 = int.Parse(args[0]);
+                string operand = args[1];
+                double num2 = int.Parse(args[2]);
+                
+                switch (operand)
+                {
+                    case "+":
+                        Console.WriteLine(num1 + num2);
+                        break;
+                    case "-":
+                        Console.WriteLine(num1 - num2);
+                        break;
+                    case "*":
+                        Console.WriteLine(num1 * num2);
+                        break;
+                    case "/":
+                        Console.WriteLine(num1 / num2);
+                        break;
+                    default:
+                        Console.WriteLine("Неудачно");
+                        break;
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введите строку в формате " +
+                    "число + (- * /) число");
+            }
         }
     }
 }
